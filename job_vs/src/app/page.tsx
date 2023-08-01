802024207a0fd299e7d17c2fdf8a238c69e210b7
lee-euiji <leeuj9663@naver.com>
2023-08-01 15:59:11 +0900

    [#3] SETTING: Color 설정 완료

--- job_vs/src/app/page.tsx
+++ job_vs/src/app/page.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <h1 className="font-HakgyoansimWoojuR tracking-wide text-8xl">
+      <h1 className="font-HakgyoansimWoojuR text-space-dark-blue tracking-wide text-8xl">
         안녕하세요
       </h1>
-      <h1 className="font-Pretendard_100 tracking-wide text-8xl">안녕하세요</h1>
+      <h1 className="font-Pretendard-900 tracking-wide text-space-magenta text-8xl">
+        안녕하세요
+      </h1>
     </main>
   );
 }
