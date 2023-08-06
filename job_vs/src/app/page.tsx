d387419ce21537fd64c1acf006fcb6ebd56a636c
lee-euiji <leeuj9663@naver.com>
2023-08-06 17:12:49 +0900

    [#7]FEAT: RecommendJD Page 완성

--- job_vs/src/app/page.tsx
+++ job_vs/src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import { FadeButton } from "@/components/fadeButton";
 import { Button } from "@/components/button";
--- job_vs/src/components/fadeButton.tsx
+++ job_vs/src/components/fadeButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ButtonProps } from "@/interfaces/components";
 import React, { useEffect, useState } from "react";
 
